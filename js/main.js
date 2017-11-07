676976eac651ed60d8a9a4a848e46c0e2d07ce39
Liam Fiddler <liamfiddler@192-168-1-7.tpgi.com.au>
2017-11-07 11:03:10 +1100

    Allow press to flip.

--- js/main.js
+++ js/main.js
@@ -4,104 +4,104 @@ const cardDesc = card.querySelector('.desc')
 const hammer = new Hammer(card)
 const flipClass = 'flipped'
 
-const values = [
-  {
-    value: '0',
-    desc: 'already done',
-  },
-  {
-    value: '½',
-    desc: 'half an hour',
-  },
-  {
-    value: '1',
-    desc: 'an hour',
-  },
-  {
-    value: '2',
-    desc: 'two hours',
-  },
-  {
-    value: '4',
-    desc: 'half a day',
-  },
-  {
-    value: '8',
-    desc: 'a day',
-  },
-  {
-    value: '16',
-    desc: 'two days',
-  },
-  {
-    value: '24',
-    desc: 'three days',
-  },
-  {
-    value: '40',
-    desc: 'a week',
-  },
-  {
-    value: '60',
-    desc: 'week and a half',
-  },
-  {
-    value: '80',
-    desc: 'a sprint',
-  },
-  {
-    value: '100',
-    desc: 'sprint and a half',
-  },
-  {
-    value: '120',
-    desc: 'two sprints',
-  },
-  {
-    value: '∞',
-    desc: 'ongoing',
-  },
-  {
-    value: '?',
-    desc: 'unestimateable',
-  },
+const values = [{
+        value: '0',
+        desc: 'already done',
+    },
+    {
+        value: '½',
+        desc: 'half an hour',
+    },
+    {
+        value: '1',
+        desc: 'an hour',
+    },
+    {
+        value: '2',
+        desc: 'two hours',
+    },
+    {
+        value: '4',
+        desc: 'half a day',
+    },
+    {
+        value: '8',
+        desc: 'a day',
+    },
+    {
+        value: '16',
+        desc: 'two days',
+    },
+    {
+        value: '24',
+        desc: 'three days',
+    },
+    {
+        value: '40',
+        desc: 'a week',
+    },
+    {
+        value: '60',
+        desc: 'week and a half',
+    },
+    {
+        value: '80',
+        desc: 'a sprint',
+    },
+    {
+        value: '100',
+        desc: 'sprint and a half',
+    },
+    {
+        value: '120',
+        desc: 'two sprints',
+    },
+    {
+        value: '∞',
+        desc: 'ongoing',
+    },
+    {
+        value: '?',
+        desc: 'unestimateable',
+    },
 ]
 
 let valueIndex = 1
 
 const flipCard = () => {
-  card.classList.toggle(flipClass)
+    card.classList.toggle(flipClass)
 
-  if ('vibrate' in navigator) {
-    navigator.vibrate(300)
-  }
+    if ('vibrate' in navigator) {
+        navigator.vibrate(300)
+    }
 }
 
 const updateCardValue = (increment = true) => {
-  if (card.classList.contains(flipClass)) {
-    return // prevent card value changing when flipped
-  }
+    if (card.classList.contains(flipClass)) {
+        return // prevent card value changing when flipped
+    }
 
-  valueIndex += increment ? 1 : -1
+    valueIndex += increment ? 1 : -1
 
-  if (valueIndex >= values.length) {
-    valueIndex = 0
-  } else if (valueIndex < 0) {
-    valueIndex = values.length - 1
-  }
+    if (valueIndex >= values.length) {
+        valueIndex = 0
+    } else if (valueIndex < 0) {
+        valueIndex = values.length - 1
+    }
 
-  const current = values[valueIndex]
+    const current = values[valueIndex]
 
-  cardValue.innerHTML = current.value
-  cardDesc.innerHTML = current.desc
+    cardValue.innerHTML = current.value
+    cardDesc.innerHTML = current.desc
 }
 
 (new Shake({
-  threshold: 10, // shake strength threshold
-  timeout: 1000 // frequency of event generation
+    threshold: 10, // shake strength threshold
+    timeout: 1000 // frequency of event generation
 })).start()
 
 window.addEventListener('shake', flipCard)
+hammer.on('press', flipCard)
 hammer.on('swipeleft', e => updateCardValue(true))
 hammer.on('swiperight', e => updateCardValue(false))
 
